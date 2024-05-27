8105c6bf5248716ab5be9a0c02f1ea69edd83127
Chithra <chithracse18@gmail.com>
2024-05-27 15:08:54 +0530

    Db Table related changes

---
+++ Database/Cabcast.Database/Auth/StoredProcedure/usp_v1_AspNetUsers_Save.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [Auth].[usp_v1_AspNetUsers_Save]
+	@UserName NVARCHAR(256),
+	@PasswordHash NVARCHAR(MAX),
+	@SecurityStamp NVARCHAR(MAX),
+	@ConcurrencyStamp NVARCHAR(MAX),
+	@PhoneNumber NVARCHAR(10),
+	@LoggedInUserId UNIQUEIDENTIFIER,
+	@Result UNIQUEIDENTIFIER OUTPUT
+AS
+DECLARE @Response INT = 0;
+
+DECLARE @EmptyGuid UNIQUEIDENTIFIER;
+SET @EmptyGuid = CAST(CAST(0 AS BINARY) AS UNIQUEIDENTIFIER);
+
+BEGIN TRY
+
+	IF([Auth].[udp_v1_ValidateLoggedInUser](@LoggedInUserId) = 0)
+	BEGIN
+		RAISERROR('INVALID_PARAM_LOGGED_IN_USER_ID', 16, 1);
+	END
+
+	IF (@UserName IS NULL OR LEN(@UserName) <= 0)
+	BEGIN
+		RAISERROR('INVALID_PARAM_USER_NAME', 16, 1);
+	END
+
+	DECLARE @AspNetUsersId UNIQUEIDENTIFIER = NEWID();
+
+    INSERT INTO [Auth].[AspNetUsers] 
+		([Id], [UserName], [NormalizedUserName], [PasswordHash], [SecurityStamp], [ConcurrencyStamp])
+	VALUES 
+		(@AspNetUsersId, @UserName, UPPER(@UserName), @PasswordHash, @SecurityStamp, @ConcurrencyStamp);
+
+	SET @Result = @AspNetUsersId;
+	RETURN @Response;
+
+END TRY  
+BEGIN CATCH  
+	DECLARE @ErrorNumber INT = ERROR_NUMBER();  
+	DECLARE @ErrorMessage NVARCHAR(1000) = ERROR_MESSAGE()
+     
+	RAISERROR('%s', 16, 1, @ErrorMessage)  
+END CATCH;
--- Database/Cabcast.Database/Auth/Table/AspNetUsers.sql
+++ Database/Cabcast.Database/Auth/Table/AspNetUsers.sql
@@ -3,13 +3,18 @@
 	[Id] NVARCHAR(36) NOT NULL,
 	[UserName] NVARCHAR(256) NULL,
 	[NormalizedUserName] NVARCHAR(256) NULL,
+	[Email] NVARCHAR(256) NULL,
+	[NormalizedEmail] NVARCHAR(256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PhoneNumber] NVARCHAR(10) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
 	[PasswordHash] NVARCHAR(MAX) NULL,
 	[SecurityStamp] NVARCHAR(MAX) NULL,
 	[ConcurrencyStamp] NVARCHAR(MAX) NULL,
-	[TwoFactorEnabled] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL DEFAULT 0,
 	[LockoutEnd] [datetimeoffset](7) NULL,
-	[LockoutEnabled] [bit] NOT NULL,
-	[AccessFailedCount] [int] NOT NULL,
+	[LockoutEnabled] [bit] NOT NULL DEFAULT 1,
+	[AccessFailedCount] [int] NOT NULL  DEFAULT 0,
 	 CONSTRAINT [PK_Auth_AspNetUsers] PRIMARY KEY CLUSTERED 
 	(
 		[Id] ASC
